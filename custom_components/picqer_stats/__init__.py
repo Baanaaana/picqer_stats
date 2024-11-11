ecdbd023044fd32a5cb240707344450536475f8c
Baanaaana <rene@rrcommerce.com>
2024-11-11 22:25:51 +0100

    deprecated fixes

--- custom_components/picqer_stats/__init__.py
+++ custom_components/picqer_stats/__init__.py
@@ -1,10 +1,19 @@
+import asyncio
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
-from .const import DOMAIN
+
+async def async_setup(hass: HomeAssistant, config: dict):
+    hass.data.setdefault("picqer_stats", {})
+    return True
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
-    hass.data[DOMAIN] = entry.data
-    hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(entry, "sensor")
-    )
+    hass.data["picqer_stats"][entry.entry_id] = entry.data
+    # Changed from async_forward_entry_setup to await async_forward_entry_setups
+    await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
     return True
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
+    unload_ok = await hass.config_entries.async_unload_platforms(entry, ["sensor"])
+    if unload_ok:
+        hass.data["picqer_stats"].pop(entry.entry_id)
+    return unload_ok
